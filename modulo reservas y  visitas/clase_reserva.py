43d014b28f2f3dfef29b4d3c321ac7cbf3ed11e8
schefferSimon <simonscheffer00@gmail.com>
2025-11-22 21:40:19 -0300

    Create clase_reserva.py

---
+++ modulo reservas y  visitas/clase_reserva.py
@@ -0,0 +1,6 @@
+class Reveserva:
+    def __init__(self, visitante, fecha , cantidad_de_personas, duracion_horas):
+        self.__visitante = visitante
+        self.__fecha = fecha
+        cantidad_de_personas
+        pass
