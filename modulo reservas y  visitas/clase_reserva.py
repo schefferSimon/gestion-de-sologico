2c9b1b40a0ce13a8a5978644c09283d1eeced3d3
schefferSimon <simonscheffer00@gmail.com>
2025-11-22 22:37:36 -0300

    exepcion personalizada

--- modulo reservas y  visitas/clase_reserva.py
+++ modulo reservas y  visitas/clase_reserva.py
@@ -1,6 +1,53 @@
-class Reveserva:
-    def __init__(self, visitante, fecha , cantidad_de_personas, duracion_horas):
-        self.__visitante = visitante
+class Reserva:
+    def __init__(self, nombreVisitante, fecha , cantidad_de_personas, duracion_horas):
+        if not isinstance(nombreVisitante, str) and nombreVisitante == "":
+            raise ValueError("nombre vacio")
+        if not len(fecha) == 3 and isinstance(fecha, tuple) :
+            raise ValueError("fecha vacia")
+        if not isinstance(cantidad_de_personas, int) and cantidad_de_personas > 0 :
+            raise ValueError("las cantidad de personas tiene que ser mayor a cero")
+
+        self.__visitante = nombreVisitante
         self.__fecha = fecha
-        cantidad_de_personas
+        self.__duracion_horas = duracion_horas
+        self.__cantidad_de_personas = cantidad_de_personas
+        self.__costo_base = 10000
+    
+    def getCosto_base(self):
+        return self.__costo_base
+        
+
+    def descripcion_reserva(self):
+        return f"Reserva normal\nel visitante: {self.__visitante}, fecha de reserva: {self.__fecha}, horas {self.__duracion_horas}, cantidad de personas: {self.__cantidad_de_personas}"
+    
+    def costo_total():
         pass
+
+class ReserveDiurna(Reserva):
+    def __init__(self, nombreVisitante, fecha, cantidad_de_personas, duracion_horas):
+        super().__init__(nombreVisitante, fecha, cantidad_de_personas, duracion_horas)
+
+    
+    def costo_total(self):
+        return self.__costo_base * 1.20
+    
+class ReservaNocturna(Reserva):
+    def __init__(self, nombreVisitante, fecha, cantidad_de_personas, duracion_horas):
+        super().__init__(nombreVisitante, fecha, cantidad_de_personas, duracion_horas)
+
+    def costo_total(self):
+        costo =  self.__costo_base * 1.35
+        
+        #tarifa minima
+        if costo < 5000:
+            costo = 5000
+        return costo
+    
+    def descripcion_reserva(self):
+        return f"Reserva Nocturna\n{super().descripcion_reserva()} costo: { self.costo_total()}"
+    
+class ReservaEducativa(Reserva):
+    def __init__(self, nombreVisitante, fecha, cantidad_de_personas, duracion_horas):
+        super().__init__(nombreVisitante, fecha, cantidad_de_personas, duracion_horas)
+
+    
