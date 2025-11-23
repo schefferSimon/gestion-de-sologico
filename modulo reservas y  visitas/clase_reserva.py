bb6c05b8298323828f63c6a17823f5f91bbb40ec
YDMS-ASIAOF <septembre323@gmail.com>
2025-11-22 23:08:02 -0300

    arreglar fecha

--- modulo reservas y  visitas/clase_reserva.py
+++ modulo reservas y  visitas/clase_reserva.py
@@ -2,13 +2,23 @@ class Reserva:
     def __init__(self, nombreVisitante, fecha , cantidad_de_personas, duracion_horas):
         if not isinstance(nombreVisitante, str) and nombreVisitante == "":
             raise ValueError("nombre vacio")
-        if not len(fecha) == 3 and isinstance(fecha, (int)) :
+        if not len(fecha) == 3 and isinstance(fecha, tuple):
             raise ValueError("fecha vacia")
+        else:
+            for i in fecha:
+                if not isinstance(i, int):
+                    raise TypeError("El contenido de la fecha debe ser entero.")
+                
+                dd = fecha(0) 
+                mm = fecha(1)
+                aa = fecha(2)
+                self.__fecha = aa + "-" + mm + "-" + dd
+                print(self.__fecha) #Para debugear
         if not isinstance(cantidad_de_personas, int) and cantidad_de_personas > 0 :
             raise ValueError("las cantidad de personas tiene que ser mayor a cero")
 
         self.__visitante = nombreVisitante
-        self.__fecha = fecha
+        #self.__fecha = fecha
         self.__duracion_horas = duracion_horas
         self.__cantidad_de_personas = cantidad_de_personas
         self.__costo_base = 10000
@@ -27,8 +37,8 @@ class Reserva:
     @staticmethod
     def validar_fecha(fecha):
         año, mes, dia = fecha
-        if len(año) == 4 and mes < 12 and dia :
-        pass
+        if len(año) == 4 and mes < 12 and dia:
+            pass
 
 class ReserveDiurna(Reserva):
     def __init__(self, nombreVisitante, fecha, cantidad_de_personas, duracion_horas):
